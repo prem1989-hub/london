ecb3b85dd7383d35dcdad7d57f2544562d208da8
prem1989-hub <143587696+prem1989-hub@users.noreply.github.com>
2023-09-20 14:18:32 +0530

    Commit pipeline

---
+++ premganesh.js
@@ -0,0 +1,13 @@
+exports.add = function (a, b) {
+    return a+b;
+}; 
+ 
+// Returns difference of two numbers
+exports.subtract = function (a, b) {
+    return a-b;
+}; 
+ 
+// Returns product of two numbers
+exports.multiply = function (a, b) {
+    return a*b;
+};
